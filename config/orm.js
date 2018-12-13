6338c0ff9d323f1c50e72ad18d4989a2a08c1ea6
Pearl <gansuvd777@gmail.com>
2018-12-13 11:15:13 -0600

    fixing connection

--- config/orm.js
+++ config/orm.js
@@ -1,47 +1,28 @@
 // Import MySQL connection.
 var connection = require("../config/connection.js");
 
-//code borrowed from week 14 exercise 18 catsapp
-
-
-///////////////////functions/////////////////////
-// Helper function for SQL syntax.
-// helper function loops through and creates an array of question marks - ["?", "?", "?"] - and turns it into a string.
-// ["?", "?", "?"].toString() => "?,?,?";
 function printQuestionMarks(num) {
   var arr = [];
-
   for (var i = 0; i < num; i++) {
     arr.push("?");
   }
-
   return arr.toString();
 }
 
 // Helper function to convert object key/value pairs to SQL syntax
 function objToSql(ob) {
   var arr = [];
-
-  // loop through the keys and push the key/value as a string int arr
   for (var key in ob) {
     var value = ob[key];
-    // check to skip hidden properties
     if (Object.hasOwnProperty.call(ob, key)) {
-      // if string with spaces, add quotations (Cheese Burg => 'Cheese Burg')
       if (typeof value === "string" && value.indexOf(" ") >= 0) {
         value = "'" + value + "'";
       }
-      // e.g. {burger_name: 'Cheese Burg'} => ["burger_name='Cheese Burg'"]
-      // e.g. {devoured: true} => ["devoured=true"]
       arr.push(key + "=" + value);
     }
   }
-
-  // translate array of strings to a single comma-separated string
   return arr.toString();
 }
-
-///////////////end functions/////////////////////
 
 // Object for all our SQL statement functions.
 var orm = {
@@ -52,16 +33,9 @@ var orm = {
       if (err) {
         throw err;
       }
-      cb(result); //do something with the returned sql query result
+      cb(result); 
     });
   },
-
-/*
-cb = function(res) {
-      cb(res); //second layer function
-    }*/
-
-  //insertOne()
   insertOne: function(table, cols, vals, cb) {
     var queryString = "INSERT INTO " + table;
 
@@ -78,12 +52,9 @@ cb = function(res) {
       if (err) {
         throw err;
       }
-
-      cb(result); //do something with the returned sql query result
+      cb(result); 
     });
   },
-  //updateOne()
-  // An example of objColVals would be {burger_name: delicious, devoured: true}
   updateOne: function(table, objColVals, condition, cb) {
     var queryString = "UPDATE " + table;
 
@@ -97,8 +68,7 @@ cb = function(res) {
       if (err) {
         throw err;
       }
-
-      cb(result); //do something with the returned sql query result
+      cb(result); 
     });
   }
 };
